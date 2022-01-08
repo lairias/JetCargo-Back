241db7c3e8b8aa0319d3340996ab987e44854498
lairias <lairias@unah.hn>
2022-01-08 11:24:39 -0600

    Generando las relacione de las tablas de Rolles, Permisos y usuarios permitidos

--- src/config/database/R_E.js
+++ src/config/database/R_E.js
@@ -1,15 +1,19 @@
 import { PA_POEPLE } from "../../models/Pa_people";
-import { PA_TypeUsers } from "../../models/Pa_typeUsers";
+import { PA_TypeUsers } from "../../models/Pa_typeUsers"; //roles
 import { USERS } from "../../models/Users";
 import { PA_ADDRES } from "../../models/Pa_addres";
 import { PA_CITIES } from "../../models/Pa_cities";
 import { PA_COUNTRIES } from "../../models/Pa_countries";
-import { PA_CUSTOMES } from "../../models/Pa_customes";
 import { PA_EMAIL } from "../../models/Pa_email";
 import { PA_PHONES } from "../../models/Pa_phones";
 import { PA_STATES } from "../../models/Pa_states";
+
+import { PA_CUSTOMES } from "../../models/Pa_customes";
 import { BO_LOCKER } from "../../models/BO_locker";
 import { Se_PASS_RESET } from "../../models/security/SE_pass_reset";
+
+
+
 import { SE_PERMISOS } from "../../models/security/SE_permisos";
 import { MODEL_HAS_PERMISOS } from "../../models/relations/MODEL_has_permisos";
 import { MODEL_HAS_ROLES } from "../../models/relations/MODEL_has_typeUser";
@@ -19,15 +23,75 @@ import { MODEL_TYPEUSER_HAS_PERMISOS } from "../../models/relations/typeusers_ha
 import { REL_PEOPLE_PHONE } from "../../models/relations/REL_people_phone";
 import { REL_PEOPLE_EMAIL } from "../../models/relations/REL_people_email";
 
-import { Sequelize } from "sequelize";
-import db from "./index";
 
+
+//Relacion de uno a uno
+/**
+ * Foo.hasOne(Bar, {
+  onDelete: 'RESTRICT',
+  onUpdate: 'RESTRICT'
+})
+ * Bar.belongsTo(Foo)
+ */
+
+//Relacion de uno a muchos
+/*
+ * Team.hasMoany(Player, {
+  onDelete: 'RESTRICT',
+  onUpdate: 'RESTRICT'
+})
+ * Player.belongsTo(Team)
+*/
+
+//Relacion de muchos a muchos 
+
+/**
+ * 
+ * Movie.belongsToMany(Actor, { through: 'ActorMovies' });
+Actor.belongsToMany(Movie, { through: 'ActorMovies' });
+ * 
+ */
+
+
+//Carga diferida
+/**
+ * const awesomeCaptain = await Captain.findOne({
+  where: {
+    name: "Jack Sparrow"
+  }
+});
+// Do stuff with the fetched captain
+console.log('Name:', awesomeCaptain.name);
+console.log('Skill Level:', awesomeCaptain.skillLevel);
+// Now we want information about his ship!
+const hisShip = await awesomeCaptain.getShip();
+// Do stuff with the ship
+console.log('Ship Name:', hisShip.name);
+ * 
+ */
+//Carga ansiosa
+/**
+const awesomeCaptain = await Captain.findOne({
+  where: {
+    name: "Jack Sparrow"
+  },
+  include: Ship
+});
+// Now the ship comes with it
+console.log('Name:', awesomeCaptain.name);
+ * 
+ */
+
+
+
+//Relaciones de personas con las tablas de informacion necesaria
 PA_EMAIL.belongsToMany(PA_POEPLE, {
   through: REL_PEOPLE_EMAIL,
   foreignKey: "COD_EMAIL",
   onDelete: "CASCADE",
   comment: "COD OF TYPE OF PEOPLE",
 });
+
 PA_POEPLE.belongsToMany(PA_EMAIL, {
   through: REL_PEOPLE_EMAIL,
   foreignKey: "COD_PEOPLE",
@@ -41,35 +105,95 @@ PA_PHONES.belongsToMany(PA_POEPLE, {
   onDelete: "CASCADE",
   comment: "COD OF TYPE OF PEOPLE",
 });
+
 PA_POEPLE.belongsToMany(PA_PHONES, {
   through: REL_PEOPLE_PHONE,
   foreignKey: "COD_PEOPLE",
   onDelete: "CASCADE",
   comment: "COD OF TYPE OF PHONES",
 });
-
-PA_TypeUsers.hasMany(USERS, {
-  foreignKey: "COD_TIPUSERS",
-  onDelete: "CASCADE",
-  comment: "COD OF TYPE OF USERS",
-});
 PA_POEPLE.hasMany(USERS, {
   foreignKey: "COD_PEOPLE",
   onDelete: "CASCADE",
   comment: "COD OF TYPE OF USERS",
 });
+
 PA_CITIES.hasMany(PA_ADDRES, {
   foreignKey: "COD_CITY",
   onDelete: "CASCADE",
   comment: "COD OF TYPE OF USERS",
 });
+
 PA_COUNTRIES.hasMany(PA_STATES, {
   foreignKey: "COD_COUNTRY",
   onDelete: "CASCADE",
   comment: "COD OF TYPE OF STATES",
 });
+
 PA_STATES.hasMany(PA_CITIES, {
   foreignKey: "COD_STATE",
   onDelete: "CASCADE",
   comment: "COD OF TYPE OF CITIES",
 });
+
+// Relaciones de tablas de roles 
+
+
+
+SE_PERMISOS.belongsToMany(USERS, {
+  through: MODEL_HAS_PERMISOS,
+  foreignKey: {
+    name: "COD_PERMISOS",
+  },
+  onDelete: "CASCADE",
+  comment: "COD OF TYPE OF PEOPLE",
+});
+
+USERS.belongsToMany(SE_PERMISOS, {
+  through: MODEL_HAS_PERMISOS,
+  foreignKey: {
+    name: "COD_USER",
+  },
+  comment: "COD OF TYPE OF EMAIL",
+  onDelete: "CASCADE",
+});
+
+
+SE_PERMISOS.belongsToMany(PA_TypeUsers, {
+  through: MODEL_TYPEUSER_HAS_PERMISOS,
+  foreignKey: {
+    name: "COD_PERMISOS",
+  },
+  onDelete: "CASCADE",
+  comment: "COD OF TYPE OF PEOPLE",
+});
+
+PA_TypeUsers.belongsToMany(SE_PERMISOS, {
+  through: MODEL_TYPEUSER_HAS_PERMISOS,
+  foreignKey: {
+    name: "COD_TYPEUSERS",
+  },
+  comment: "COD OF TYPE OF EMAIL",
+  onDelete: "CASCADE",
+});
+
+
+PA_TypeUsers.belongsToMany(USERS, {
+  through: MODEL_HAS_ROLES,
+  foreignKey: {
+    name: "COD_TYPEUSERS",
+  },
+  onDelete: "CASCADE",
+  comment: "COD OF TYPE OF PEOPLE",
+});
+
+USERS.belongsToMany(PA_TypeUsers, {
+  through: MODEL_HAS_ROLES,
+  foreignKey: {
+    name: "COD_USER",
+  },
+  comment: "COD OF TYPE OF EMAIL",
+  onDelete: "CASCADE",
+});
+
+
