16c44b2c75aa06431aecf1400c41cafd0f1ea2f7
lairias <54221775+lairias@users.noreply.github.com>
2022-03-26 23:16:47 -0600

    Mejora de permisos

---
+++ src/middleware/permissions/index.js
@@ -0,0 +1,16 @@
+import sequelize from "../../config/database";
+import { HttpError } from "../../helpers/handleError";
+export const VeryPermission = async () => {
+  try {
+    const permiso = await sequelize.query(
+      "CALL SHOW_PERMISOS_USER(:COD_USER,:NAM_PERMISO)",
+      { replacements: { COD_USER: req.userId, NAM_PERMISO: "addres.view" } }
+    );
+    if (!JSON.stringify(permiso[0]))
+      return res.status(403).json({ message: "Acceso no Autorizado" });
+    next();
+  } catch (error) {
+    HttpError(res, error);
+    next();
+  }
+};
