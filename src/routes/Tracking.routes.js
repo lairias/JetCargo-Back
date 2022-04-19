8c1f2792f9e5a37c8b4d2cce44b02e03eea34c15
lairias <54221775+lairias@users.noreply.github.com>
2022-04-18 21:07:06 -0600

    empy

--- src/routes/Tracking.routes.js
+++ src/routes/Tracking.routes.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const {
-  // CreateTracking,
-  // GetTrackingsNumberService,
-  // GetTrackingsNumber,
-  // UpdateTracking,
-  // GetTracking_not_orden,
+  CreateTracking,
+  GetTrackingsNumberService,
+  GetTrackingsNumber,
+  UpdateTracking,
+  GetTracking_not_orden,
   TrackingNotOrdenType,
-  // TrackingNotOrden,
+  TrackingNotOrden,
   TrackingNotOrdenCustomer,
-  // GetTracking,
+  GetTracking,
 } =require( "../controllers/Tracking.Controllers")
 const { verifyIndUser, verifyToken } =require( "../middleware/verifySignup")
 
-// router.get(
-//   "/search/:COD_SERVICE/:NUM_TRACKING",
-//   [verifyToken, verifyIndUser],
-//   GetTrackingsNumberService
-// );
-// router.get(
-//   "/search/:NUM_TRACKING",
-//   [verifyToken, verifyIndUser],
-//   GetTrackingsNumber
-// );
+router.get(
+  "/search/:COD_SERVICE/:NUM_TRACKING",
+  [verifyToken, verifyIndUser],
+  GetTrackingsNumberService
+);
+router.get(
+  "/search/:NUM_TRACKING",
+  [verifyToken, verifyIndUser],
+  GetTrackingsNumber
+);
 
-// router.get(
-//   "/received/:RECEIVED_TRACKING",
-//   [verifyToken, verifyIndUser],
-//   TrackingNotOrden
-// );
+router.get(
+  "/received/:RECEIVED_TRACKING",
+  [verifyToken, verifyIndUser],
+  TrackingNotOrden
+);
 router.get(
   "/received/:RECEIVED_TRACKING/:COD_CUSTOMER/:NUM_TRACKING",
   [verifyToken, verifyIndUser],
@@ -41,14 +41,14 @@ router.get(
   TrackingNotOrdenType
 );
 
-// router.get(
-//   "/:COD_TRACKING/:COD_PACKAGE/:NUM_LOCKER/:COD_CUSTOMER",
-//   [verifyToken, verifyIndUser],
-//   GetTracking_not_orden
-// );
-// router.get("/:NUM_TRACKING", [verifyToken, verifyIndUser], GetTrackingsNumber);
-// router.get("/GetBytracking/:COD_TRACKING", [verifyToken, verifyIndUser], GetTracking);
-// router.post("/", [verifyToken, verifyIndUser], CreateTracking);
-// router.put("/:COD_TRACKING", [verifyToken, verifyIndUser], UpdateTracking);
+router.get(
+  "/:COD_TRACKING/:COD_PACKAGE/:NUM_LOCKER/:COD_CUSTOMER",
+  [verifyToken, verifyIndUser],
+  GetTracking_not_orden
+);
+router.get("/:NUM_TRACKING", [verifyToken, verifyIndUser], GetTrackingsNumber);
+router.get("/GetBytracking/:COD_TRACKING", [verifyToken, verifyIndUser], GetTracking);
+router.post("/", [verifyToken, verifyIndUser], CreateTracking);
+router.put("/:COD_TRACKING", [verifyToken, verifyIndUser], UpdateTracking);
 
 module.exports = router;
